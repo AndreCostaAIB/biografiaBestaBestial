112670b4ae8f3f862395046f447bfde8bc604831
AndreCostaAIB <36609840+AndreCostaAIB@users.noreply.github.com>
2018-10-08 23:55:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+function funcao1(){window.location.href ="https://www.youtube.com/watch?v=9kp4BinvdRk&feature=youtu.be"}
+function funcao2(){window.location.href ="https://andrecostaaib.github.io/PTGeral7/"}
